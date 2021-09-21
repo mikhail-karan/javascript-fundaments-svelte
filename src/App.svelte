8d8454516cfbf94bf40488aa9b31c420a8380f28
Mikhail Karan <mikhail.karan@digitaldynasty.ca>
2021-09-21 13:30:40 -0400

    Reactivity using a add and multiply commands

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,24 @@
 <script>
+  let numA = 1
+
+  function add5(){
+    numA += 5
+  }
+
+  $: multiple = numA*2
+
 
 </script>
 
-<main>
-
+<main class="flex w-full flex-col items-center mt-10 justify-center">
+  <div class="prose lg:prose-xl text-center">
+    <h2 class="text-white">Lets get Reactive</h2>
+    <p>This is numA: {numA}</p>
+    <p>This is numA * 2: {multiple}</p>
+    <div class="btn btn-primary" on:click="{add5}">Add 5 to numA</div>
+  </div>
 </main>
 
 <style>
-  
+
 </style>
