864e07019798474deb06724a7cd3d3e4afae0cbd
Mikhail Karan <mikhail.karan@digitaldynasty.ca>
2021-09-23 13:18:25 -0400

    added delete to posts to test lifecycle events

--- src/App.svelte
+++ src/App.svelte
@@ -15,12 +15,18 @@
     posts.push(postObj);
     posts = posts;
   }
+
+  function deletePost(id){
+    const deleteIndex = posts.findIndex(post => post.id === id)
+    posts.splice(deleteIndex, 1)
+    posts = posts;
+  }
 </script>
 
 <main class="flex w-full flex-col items-center mt-10 justify-center space-y-5">
   <AddForm addPost={addPost} />
   {#each posts.reverse() as post}
-    <Post author={post.author} post={post.post} />
+    <Post deleteItem={deletePost} author={post.author} post={post.post} />
   {/each}
 </main>
 
--- src/components/Post.svelte
+++ src/components/Post.svelte
@@ -1,9 +1,25 @@
 <script>
- export let author;
- export let post;
+  import {onMount} from 'svelte'
+  export let author;
+  export let post;
+  export let id;
+  export let deleteItem;
+
+  onMount(() => {
+    console.log(author, ' post mounted')
+
+    return () => console.log('post unmounted')
+  })
 </script>
+
 <!-- https://daisyui.com/components/card/ -->
 <div class="card bg-neutral text-white w-80 shadow-xl">
+  <div
+    on:click={deleteItem(id)}
+    class="absolute right-0 pr-2 text-error text-2xl"
+  >
+    x
+  </div>
   <div class="card-body">
     <h2 class="card-title">{author}</h2>
     <p>{post}</p>
