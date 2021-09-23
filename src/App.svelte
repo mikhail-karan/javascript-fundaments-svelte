75dfa0e952d06d2f945a3e38821b8ce631efc7d9
Mikhail Karan <mikhail.karan@digitaldynasty.ca>
2021-09-23 14:31:38 -0400

    added explanations

--- src/App.svelte
+++ src/App.svelte
@@ -3,14 +3,17 @@
 
   let posts = data.data
 
+
 </script>
 
 <main class="flex w-full flex-col items-center mt-10 justify-center space-y-5">
   <!-- https://daisyui.com/components/card/ -->
+  <!-- mention we are declaring post as a new variable that can be any name. It is not auto generated -->
   {#each posts as post }
     <div class="card bg-neutral text-white w-80 shadow-xl">
       <div class="card-body">
         <h2 class="card-title">{post.author}</h2> 
+        <!-- Parent and child can have the same name -->
         <p>{post.post}</p> 
       </div>
     </div>
