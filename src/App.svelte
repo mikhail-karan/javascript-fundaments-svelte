d5235528888c735a0d0882dacb5de5ee2c0dd4c0
Mikhail Karan <mikhail.karan@digitaldynasty.ca>
2021-09-23 14:23:45 -0400

    changed numbers to match active

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,20 @@
 <script>
-  let active = 0;
+  let active = 1;
 </script>
 
 <main class="flex w-full flex-col items-center mt-10 justify-center">
   <div class="btn-group">
     <button 
-      on:click="{() => active = 0}"
-      class="btn btn-lg {active === 0 ? 'btn-active' : ''}">
-      One</button>
-    <button 
       on:click="{() => active = 1}"
       class="btn btn-lg {active === 1 ? 'btn-active' : ''}">
+      One</button>
+    <button 
+      on:click="{() => active = 2}"
+      class="btn btn-lg {active === 2 ? 'btn-active' : ''}">
       Two</button>
     <button
-      on:click="{() => active = 2}" 
-      class="btn btn-lg {active === 2 ? 'btn-active' : ''}">
+      on:click="{() => active = 3}" 
+      class="btn btn-lg {active === 3 ? 'btn-active' : ''}">
       Three</button>
   </div>
 </main>
