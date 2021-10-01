40ff8a1e2037872846fbefe3eaa8e4e03e8539a0
Mikhail Karan <mikhail.karan@digitaldynasty.ca>
2021-10-01 16:23:55 -0400

    on post timer added for mount and destroy example

--- src/components/Post.svelte
+++ src/components/Post.svelte
@@ -1,17 +1,31 @@
 <script>
-  import {onMount} from 'svelte'
+  import { onMount, onDestroy, beforeUpdate, tick } from "svelte";
   export let author;
   export let post;
   export let id;
   export let deleteItem;
 
-  onMount(() => {
-    console.log(id, ' post mounted')
+  let timeOnScreen = 0;
+  let timeInterval = null;
 
-    return () => {
-      console.log('post deleted')
-    }
-  })
+  beforeUpdate(async () => {
+  	console.log('the component is about to update');
+  	await tick();
+  	console.log('the component just updated');
+  });
+
+  onMount(() => {
+    console.log("Post Created");
+    timeInterval = setInterval(() => {
+      ++timeOnScreen;
+      console.log(`Interval updated for post id ${id}`)
+    },1000);
+    // initDestroy(id, author)
+  });
+
+  onDestroy(() => {
+    clearInterval(timeInterval);
+  });
 </script>
 
 <!-- https://daisyui.com/components/card/ -->
@@ -23,7 +37,7 @@
     x
   </div>
   <div class="card-body">
-    <h2 class="card-title">{author}</h2>
+    <h2 class="card-title">{author} {timeOnScreen}</h2>
     <p>{post}</p>
   </div>
 </div>
